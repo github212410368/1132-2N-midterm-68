99266bab19fbcd23de34758253ee58bcce3c05f1
ting <212410368@o365.tku.edu.tw>
2025-04-16 21:50:30 +0800

    2

--- app/demo_68/w06_blog_68/page.js
+++ app/demo_68/w06_blog_68/page.js
@@ -1,12 +1,12 @@
-import { getBlogs } from '@/lib/db-blog';
-import Blogs_68 from '@/components/demo_68/w06_blogs_68/Blogs_68';
+// import { getBlogs } from '@/lib/db-blog';
+// import Blogs_68 from '@/components/demo_68/w06_blogs_68/Blogs_68';
 
-export default async function P3Page_68() {
-  const blogs = await getBlogs();
-  console.log('blogs', JSON.stringify(blogs));
-  return (
-    <>
-      <Blogs_68 blogs={blogs} />
-    </>
-  );
-}
+// export default async function P3Page_68() {
+//   const blogs = await getBlogs();
+//   console.log('blogs', JSON.stringify(blogs));
+//   return (
+//     <>
+//       <Blogs_68 blogs={blogs} />
+//     </>
+//   );
+// }
--- components/demo_68/NavigationDemo_68.js
+++ components/demo_68/NavigationDemo_68.js
@@ -1,42 +1,42 @@
-import Link from 'next/link';
+// import Link from 'next/link';
 
-export default function NavigationDemo_68() {
-  return (
-    <nav className='z-10 text-xl'>
-      <ul className='flex gap-16 items-center'>
-        <li>
-          <Link
-            href='/demo_68/w02_tailwind_68'
-            className='hover:text-accent-400 transition-colors'
-          >
-            w2_tailwind_68
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/demo_68/w05_oasis_68'
-            className='hover:text-accent-400 transition-colors'
-          >
-            w5_oasis_68
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/demo_68/w06_blog_68'
-            className='hover:text-accent-400 transition-colors'
-          >
-            w6_blog_68
-          </Link>
-        </li>
-        <li>
-          <Link
-            href='/demo_68/w08_booklist_68'
-            className='hover:text-accent-400 transition-colors'
-          >
-            w8_booklist_68
-          </Link>
-        </li>
-      </ul>
-    </nav>
-  );
-}
+// export default function NavigationDemo_68() {
+//   return (
+//     <nav className='z-10 text-xl'>
+//       <ul className='flex gap-16 items-center'>
+//         <li>
+//           <Link
+//             href='/demo_68/w02_tailwind_68'
+//             className='hover:text-accent-400 transition-colors'
+//           >
+//             w2_tailwind_68
+//           </Link>
+//         </li>
+//         <li>
+//           <Link
+//             href='/demo_68/w05_oasis_68'
+//             className='hover:text-accent-400 transition-colors'
+//           >
+//             w5_oasis_68
+//           </Link>
+//         </li>
+//         <li>
+//           <Link
+//             href='/demo_68/w06_blog_68'
+//             className='hover:text-accent-400 transition-colors'
+//           >
+//             w6_blog_68
+//           </Link>
+//         </li>
+//         <li>
+//           <Link
+//             href='/demo_68/w08_booklist_68'
+//             className='hover:text-accent-400 transition-colors'
+//           >
+//             w8_booklist_68
+//           </Link>
+//         </li>
+//       </ul>
+//     </nav>
+//   );
+// }
